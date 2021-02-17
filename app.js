561678093bef161160b5f8d8388de8198a3302eb
zachSelindh <zachselindh@gmail.com>
2021-02-17 08:52:16 -0600

    Working with tuples.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var person = {
     name: "Zach",
     age: 30,
-    hobbies: ['Coding', 'Guitar']
+    hobbies: ['Coding', 'Guitar'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+/* person.role[1] = 10; */
 var favoriteActivities;
 favoriteActivities = ['sports'];
-console.log(person.name);
+console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
--- app.ts
+++ app.ts
@@ -1,13 +1,22 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
     name: "Zach",
     age: 30,
-    hobbies: ['Coding', 'Guitar']
+    hobbies: ['Coding', 'Guitar'],
+    role: [2, 'author']
 };
+
+person.role.push('admin')
+/* person.role[1] = 10; */
 
 let favoriteActivities: string[];
 favoriteActivities = ['sports']
 
-console.log(person.name);
+console.log(person);
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
